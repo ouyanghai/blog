af6a5d2f72164efff8e1cc8429674ef5ad0d525d
ouyanghai <1114374850@qq.com>
2015-12-16 14:02:44 +0800

    更改调适模式为false

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header("content-type:text/html;charset=utf-8");
 $yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG',false);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
