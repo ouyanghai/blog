0c91d32fcb8dedd3467db66bca7a7a9a4b6eab49
ouyanghai <1114374850@qq.com>
2015-12-16 15:58:21 +0800

    修改了主页背景图片与文字

--- protected/views/Index/index.php
+++ protected/views/Index/index.php
@@ -1,24 +1,17 @@
 <ul class="bloglist">
 
 <div class="info">
-    <figure> <img src="<?php echo Yii::app()->request->baseUrl ?>/assets/home/images/art.jpg"  alt="Panama Hat">
-      <figcaption><strong>渡人如渡己，渡已，亦是渡</strong> 当我们被误解时，会花很多时间去辩白。 但没有用，没人愿意听，大家习惯按自己的所闻、理解做出判别，每个人其实都很固执。与其努力且痛苦的试图扭转别人的评判，不如默默承受，给大家多一点时间和空间去了解。而我们省下辩解的功夫，去实现自身更久远的人生价值。其实，渡人如渡己，渡已，亦是渡人。</figcaption>
-    </figure>
-    
-  </div>
+    <div class="mainfigure">
+      <img src="<?php echo Yii::app()->request->baseUrl ?>/assets/home/images/in.jpg"  alt="Panama Hat">
+        <div class="mainfigcaption">
+            <strong>渡人，渡已</strong> 
+            <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;生命，是一场孤独的跋涉，一个人走，一个人跑，一个人流浪;一个人哭，一个人笑，一个人坚强。一场磨难，是一场洗礼;一场伤痛，是一场觉醒。走过，累过，哭过，才会成长;痛苦过，悲伤过，寂寞过，才会飞翔.
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;有生命的日子，每一天都应该是崭新的自己，无论道路坎坷或曲折，都应该在有生的日子里好好珍惜。人生的路，没有一帆风顺，即使脚下的路程在怎么艰辛，生活还得依旧继续，道路还得依旧行走。其实，人生就是一场梦境，不必在乎太多，也不必为自己的失败而气妥，生命终归是自己的，没有人可以主宰你，珍惜生命，善待自己，才是生活最好的唯一!</p>
 
-  <div class="info">
-    <figure> <img src="<?php echo Yii::app()->request->baseUrl ?>/assets/home/images/art.jpg"  alt="Panama Hat">
-      <figcaption><strong>渡人如渡己，渡已，亦是渡</strong> 当我们被误解时，会花很多时间去辩白。 但没有用，没人愿意听，大家习惯按自己的所闻、理解做出判别，每个人其实都很固执。与其努力且痛苦的试图扭转别人的评判，不如默默承受，给大家多一点时间和空间去了解。而我们省下辩解的功夫，去实现自身更久远的人生价值。其实，渡人如渡己，渡已，亦是渡人。</figcaption>
-    </figure>
-    
-  </div>
-
-  <div class="info">
-    <figure> <img src="<?php echo Yii::app()->request->baseUrl ?>/assets/home/images/art.jpg"  alt="Panama Hat">
-      <figcaption><strong>渡人如渡己，渡已，亦是渡</strong> 当我们被误解时，会花很多时间去辩白。 但没有用，没人愿意听，大家习惯按自己的所闻、理解做出判别，每个人其实都很固执。与其努力且痛苦的试图扭转别人的评判，不如默默承受，给大家多一点时间和空间去了解。而我们省下辩解的功夫，去实现自身更久远的人生价值。其实，渡人如渡己，渡已，亦是渡人。</figcaption>
-    </figure>
-    
+        </div>
+    </div>
   </div>
 
 </ul>
